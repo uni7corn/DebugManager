da03d6369b93b1b9708150810d5e3bb64fdf7983
stephenzhan <zhanfeng990927@gmail.com>
2025-02-17 16:44:10 +0800

    refactor:升级版号

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias (libs.plugins.kotlinx.serialization)
 }
 
-val debugManagerAppVersion = "1.2.2"
+val debugManagerAppVersion = "1.2.3"
 
 group = "com.stephen"
 version = debugManagerAppVersion
