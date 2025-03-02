b64667df674e34e4f939f51a82fee20b3b4ed6d4
stephenzhan <zhanfeng990927@gmail.com>
2025-03-02 19:33:37 +0800

    refactor:升级版号，gradle注释掉屏蔽检查JDK发行商的行

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias (libs.plugins.kotlinx.serialization)
 }
 
-val debugManagerAppVersion = "1.4.1"
+val debugManagerAppVersion = "1.4.2"
 
 group = "com.stephen"
 version = debugManagerAppVersion
