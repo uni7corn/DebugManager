2732cdde16fdc52fec625f6af9b7e2f920e55887
stephenzhan <zhanfeng990927@gmail.com>
2025-08-29 11:50:56 +0800

    Update FileManagePage.kt

--- composeApp/src/desktopMain/kotlin/com/stephen/debugmanager/ui/pages/FileManagePage.kt
+++ composeApp/src/desktopMain/kotlin/com/stephen/debugmanager/ui/pages/FileManagePage.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
