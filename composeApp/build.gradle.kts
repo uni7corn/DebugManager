ca1f887c831221cde444b850b900af73c455eb65
stephenzhan <zhanfeng990927@gmail.com>
2025-09-05 23:58:42 +0800

    chore: Bump version to 2.6.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias (libs.plugins.kotlinx.serialization)
 }
 
-val debugManagerAppVersion = "2.5.4"
+val debugManagerAppVersion = "2.6.0"
 
 group = "com.stephen"
 version = debugManagerAppVersion
